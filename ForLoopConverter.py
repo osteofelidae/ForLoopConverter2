eaa372c0a8739ca2559a453ef1b34f31ae17fbd4
osteofelidae <azrieldelta@gmail.com>
2022-10-18 11:40:14 -0700

    mid changes

--- ForLoopConverter.py
+++ ForLoopConverter.py
@@ -1,1 +1,26 @@
-hhh
+import string
+
+def checkVar(varIndex, strInput):
+    strInput = " " + strInput + " "
+    if not((strInput[varIndex]) in string.ascii_lowercase) and not((strInput[varIndex+2]) in string.ascii_lowercase):
+        return True
+    else:
+        return False
+    
+def findVars(varName, strInput):
+    indexList = []
+    for checkIndex in range(len(strInput)):
+        if checkVar(checkIndex, strInput) and (strInput[checkIndex] == varName):
+            indexList.append(checkIndex)
+    return indexList
+
+
+
+inFileName = input("Input input file path... ")
+outFileName = input("Input output file path...")
+
+inFile = open(inFileName, "r+")
+
+print(inFile.readlines)
+
+inFile.close()
